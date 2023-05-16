75656a62c11e771fbd35ef9fb814b9e1cb38af02
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-05-16 14:17:30 -0300

    Create todoitem file

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -6,20 +6,20 @@ export function TodoList(todos){
             return (
                 <li key={todo.id}>
 
-                <label>
-                    <input type="checkbox" 
-                    checked={todo.completed}
-                    //onChange={e => toggleTodo(todo.id, e.target.checked)}
-                    />
-                    {todo.title}
-                </label>
+                    <label>
+                        <input type="checkbox" 
+                        checked={todo.completed}
+                        //onChange={e => toggleTodo(todo.id, e.target.checked)}
+                        />
+                        {todo.title}
+                    </label>
 
-                <button 
-                    //onClick={() => deleteTodo(todo.id)} 
-                    className="btn btn-danger"
-                >
-                    Delete
-                </button>
+                    <button 
+                        //onClick={() => deleteTodo(todo.id)} 
+                        className="btn btn-danger"
+                    >
+                        Delete
+                    </button>
                 </li>
             )
             })}
