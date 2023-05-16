23ed7b3b3f7f4d986e6bd4cfd146d3ec8797ece3
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-05-16 14:39:58 -0300

    Some gramma update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
-import "./styles.css"
 import { NewTodoForm } from "./NewTodoForm"
+import "./styles.css"
 import { TodoList } from "./TodoList"
 
-export default function App(){
+export default function App() {
   const [todos, setTodos] = useState(() => {
     const localValue = localStorage.getItem("ITEMS")
     if (localValue == null) return []
@@ -15,39 +15,38 @@ export default function App(){
     localStorage.setItem("ITEMS", JSON.stringify(todos))
   }, [todos])
 
-  function addTodo(title){
-    setTodos(currentTodos =>{
+  function addTodo(title) {
+    setTodos(currentTodos => {
       return [
         ...currentTodos,
-        {id: crypto.randomUUID(), title, completed: false},
+        { id: crypto.randomUUID(), title, completed: false },
       ]
     })
   }
 
-  function toggleTodo(id, completed){
-    setTodos(currentTodos =>{
-      return currentTodos.map(todo =>{
-        if(todo.id === id){
-          return {...todo, completed}
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
         }
+
         return todo
       })
     })
   }
 
-  //delete the input todo
-  function deleteTodo(id){
-    setTodos(currentTodos =>{
-      //if the id didn't match, then keep it; otherwise remove it(not return it)
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
     })
   }
 
-  return(
+  return (
     <>
-      <NewTodoForm onSubmit={addTodo}/>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
